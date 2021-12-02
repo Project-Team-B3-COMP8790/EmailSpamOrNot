0f3edef9d391830ed6c3690a4dd46fb6f95f3ca7
Suhail Singh Bains <bains.singh.suhail@gmail.com>
2021-12-02 17:48:21 -0500

    Added Naive Bayes

--- svm.py
+++ svm.py
@@ -3,10 +3,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn import svm
 from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Load Data
 dataframe = pd.read_csv("spam.csv")
 print(dataframe.describe())
+dataframe = dataframe.sample(frac = 1)
 
 # Split Test & Training
 
@@ -21,8 +23,8 @@ cv = CountVectorizer()
 features = cv.fit_transform(x_train)
 
 # Model 
-tuned_parameters = {'kernel': ['rbf','linear'], 'gamma': [1e-3, 1e-4],
-                     'C': [1, 10, 100, 1000]}
+# tuned_parameters = {'kernel': ['rbf','linear'], 'gamma': [1e-3, 1e-4], 'C': [1, 10, 100, 1000]}
+tuned_parameters =   {'kernel': ['rbf','linear'],'gamma': ['auto']}
 
 model = GridSearchCV(svm.SVC(), tuned_parameters)
 
@@ -32,6 +34,3 @@ print(model.best_params_)
 
 # Show Accuracy
 print(model.score(cv.transform(x_test),y_test))
-
-
-
